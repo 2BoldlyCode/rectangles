281b6d03a7458ad4296306d1b2899e294416f089
Cody McCain <cody@2boldlycode.com>
2016-09-29 07:08:29 -0400

    added version info to output, script working with docker

--- rectangles-cli/src/main/java/com/apprenda/rectangles/cli/Application.java
+++ rectangles-cli/src/main/java/com/apprenda/rectangles/cli/Application.java
@@ -2,19 +2,26 @@ package com.apprenda.rectangles.cli;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.apprenda.rectangles.RectanglesAnalyzer;
+import com.apprenda.rectangles.Version;
 import com.apprenda.rectangles.model.AnalysisResult;
 import com.apprenda.rectangles.model.Rectangle;
 import com.apprenda.rectangles.output.RectanglesTextWriter;
 import com.apprenda.rectangles.parse.WellKnownTextParser;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = Version.class)
 public class Application implements CommandLineRunner {
-
+	
+	@Autowired
+	private Version version;
+	
 	public static void main(String[] args) throws Exception {
 		SpringApplication app = new SpringApplication(Application.class);
 		app.setHeadless(true);
@@ -25,6 +32,7 @@ public class Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		System.out.println(version.toString());
 		final WellKnownTextParser parser = new WellKnownTextParser(System.in);
 		final RectanglesTextWriter writer = new RectanglesTextWriter(System.out);
 
